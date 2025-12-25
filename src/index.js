1517a902287002a89b6d0c963e211d65ccb12526
Brian MacIntosh <brianamacintosh@gmail.com>
2025-12-25 12:42:08 -0800

    sort subgroups by start time

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 const { Timeline } = require("vis-timeline")
 const { DataSet } = require("vis-data")
+const moment = require('moment')
 import 'vis-timeline/styles/vis-timeline-graph2d.css'
 import 'vis-chronicle/styles/style.css'
 import './style.css'
 const timelineData = require("../intermediate/timeline.json")
 
 var container = document.getElementById("visualization")
+
+timelineData.groups[0].subgroupOrder = function(a, b)
+{
+	//TODO: what determines which subgroup event is passed?
+	return moment(a.start).valueOf() - moment(b.start).valueOf()
+}
 
 var groups = new DataSet(timelineData.groups)
 var items = new DataSet(timelineData.items)
